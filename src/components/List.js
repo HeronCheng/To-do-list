63b3048334a6555b9734248e66f8835a6f8c171c
Heron Cheng <u101003418@cmu.edu.tw>
2022-04-28 22:37:38 +0800

    8-7 commit

--- src/components/List.js
+++ src/components/List.js
@@ -1,23 +1,19 @@
 import React from "react";
 import ToDo from "./ToDo";
-import {database, ref,remove}  from "../firebaseConfig";
+
 
 const List = ({toDoList,setToDoList}) => {
 
-	let toDoValue=toDoList.map(item => item.task);
+	// let toDoValue=toDoList.map(item => item.task);
 
-	const deleteItem = (id) => {
-		console.log(id)
-		const handleDeleteRef=ref(database,"/");
-		remove(handleDeleteRef,id);
-	};
+	
 	
 	return (
 		<React.Fragment >
 			{toDoList.map(todo => {
 				return (
 					<>
-					<ToDo todo={todo} deleteItem={deleteItem} key={Math.random()} />
+					<ToDo todo={todo}  key={todo.id} />
 					<div className="border-solid border-slate-200 border-b-2 w-1/3 mx-auto"></div>
 					</>
 				);
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,23 @@
 import React from "react";
 import deleteicon from "../img/garbage-can.png";
-
+import {database, ref,remove}  from "../firebaseConfig";
 
 const ToDo = (props) => {
-	const {todo, deleteItem}=props;
+	const {todo}=props;
+	
+	const deleteItem = (id) => {
+		console.log(id)
+		const handleDeleteRef=ref(database,"/");
+		remove(handleDeleteRef,id);
+	};
 	const handleDelete = (e) => {
 		e.preventDefault();
 		e.stopPropagation();
+		console.log(e.currentTarget.parentNode.id)
 		deleteItem(e.currentTarget.parentNode.id);
 	};
+
+
 	return (
 		<React.Fragment >
 			<div className={todo.complete ? "hidden" : "flex items-center justify-center"}>
